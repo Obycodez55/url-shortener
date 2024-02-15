e2dac85293594bb09eb53e21c2360738fbfb1823
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-02-15 05:16:51 +0100

    added favicon and changed some styleings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,31 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        primary:{
-          500: "#CC59D2",
+      colors: {
+        primary: {
+          100: "#f5def6",
+          200: "#ebbded",
+          300: "#e09be4",
+          400: "#d67adb",
+          500: "#cc59d2",
+          600: "#a347a8",
+          700: "#7a357e",
+          800: "#522454",
+          900: "#29122a"
         },
-        secondary:{
-          500:"#7FEFBD",
-        },
-      },
-    },
+        secondary: {
+          100: "#e5fcf2",
+          200: "#ccf9e5",
+          300: "#b2f5d7",
+          400: "#99f2ca",
+          500: "#7fefbd",
+          600: "#66bf97",
+          700: "#4c8f71",
+          800: "#33604c",
+          900: "#193026"
+        }
+      }
+    }
   },
-  plugins: [require('@tailwindcss/forms')],
-}
-
+  plugins: [require("@tailwindcss/forms")]
+};
